8e2faa5e5d93d41cda20c3ab682f183588337e55
Marcus Behel <marcus.behel@mindspring.com>
2023-11-06 23:27:25 -0500

    Using clang and lld

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ sudo mount --bind $sysrootdest $jetsonroot/mnt/sysroot
 Copy the convert-to-sysroot script to the chroot
 
 ```sh
-sudo cp make-cross-toolchain/convert-to-sysroot.sh $jetsonroot/root/
+sudo cp convert-to-sysroot.sh $jetsonroot/root/
 ```
 
 Enter the chroot and run the script. This script will copy necessary files, fix symlinks to be relative (so they are useable in a portable sysroot), the report any broken links remaining (many of these won't need to be fixed because they linked to things that were intentionally omitted or they were broken to begin with).
@@ -69,52 +69,3 @@ Then, pack an archive of the sysroot. The sysroot will probably be quite large (
 tar -cvf sysroot-jetson.tar *
 xz -z -T0 -v sysroot-jetson.tar
 ```
-
-
-## Making Toolchain (Linux AMD64 host)
-
-*Note: Build in a chroot for a system with same (or older) GCC, GLIBC, and BINUTILS version to your target. Minimizes risk of not being able to build GCC. Generally, a version of GCC can build the same version of GCC without issues (also usually ok to build GCC x with a few versions older than x). Tested in Ubuntu 20.04 chroot. Also note that system you build on defines glibc compatibility of generated binaries. Could build on 18.04 if older glibc compat is necessary.*
-
-You must have a sysroot first.
-
-Change versions to match what is used on your target OS.
-
-```sh
-tcbuild=$HOME/toolchain-jetson-build
-tcdest=$HOME/toolchain-jetson-linux-amd64
-sysrootdir=$HOME/sysroot-jetson
-
-# Change for non-debian based build system
-sudo apt-get install -y build-essential texinfo
-
-mkdir -p $tcbuild
-mkdir -p $tcdest
-cp make-cross-toolchain/tc*.sh $tcbuild
-cd $tcbuild
-BINUTILS_VER=2.34 SYSROOT=$sysrootdir TOOLCHAIN_DIR=$tcdest ./tc-build-binutils.sh
-GCC_VER=9.4.0 SYSROOT=$sysrootdir TOOLCHAIN_DIR=$tcdest ./tc-build-gcc.sh
-```
-
-Finally, package it
-
-```sh
-cd $tcdest
-tar -cvf toolchain-jetson-linux-amd64.tar *
-xz -z -T0 -v toolchain-jetson-linux-amd64.tar
-```
-
-## Non-Linux host systems
-
-Not supported (meaning I haven't made it work). It could, but there are some considerations. It's not trivial.
-
-- Sysroot:
-    - Uses unix symlinks. Should work on macos. Won't on windows
-    - Would need to replace symlinks with copies for windows
-    - This would make sysroot much larger.
-- Toolchain:
-    - Would need canadian cross compiler build process (host, target, and build systems all different)
-    - I don't want to figure this out without using crosstool-ng
-    - The problem with crosstool-ng is that you can't build it against an existing sysroot, which is necessary (kinda) in this case. Technically, you could have it make its own sysroot, but it may have linking issues with glibc / other builtin libs
-    - It could be possible to build a toolchain with crosstool-ng with its own sysroot and just modify include / library paths to use real sysroot (but not actually use syroot flag). This would let gcc link against its own glibc. As long as glibc is same (or older) than used in real sysroot, it should be fine (gcc version should also probably be the same).
-    - Better option would be to manually build canadian cross toolchain on Linux host using osxcross to have macos host and mingw-w64 to have windows host. Not really sure what this would entail. May just require adding --build flag? I may experiment with it eventually.
-    - Another option could be to combine the jetson sysroot's libraries and includes with the crosstool-ng sysroot without overwriting crosstool-ng sysroot's files. This again *should* be fine with same glibc and gcc versions (or older glibc). Maybe this is cleaner overall? Just collect libraries from wherever they are on jetson image and merge into sysroot from ct-ng toolchain? Maybe easier? Maybe not though. have to look into what canadian cross entails.
--- buildjetson.sh
+++ buildjetson.sh
@@ -1,63 +1,80 @@
 #!/usr/bin/env bash
 
 ################################################################################
-# Default settings
+# Setup
 ################################################################################
-# Default values if these are not specified via env vars
+# Default value if not specified via env var
 [ -z "$JETSON_SYSROOT" ] && JETSON_SYSROOT=$HOME/sysroot-jetson
-[ -z "$JETSON_TOOLCHAIN" ] && JETSON_TOOLCHAIN=$HOME/toolchain-jetson/
-[ -z "$JETSON_TOOLCHAIN_PREFIX" ] && JETSON_TOOLCHAIN_PREFIX=aarch64-linux-gnu-
 ################################################################################
 
+
+################################################################################
+# Flags passed to clang
+################################################################################
+# Passed to everything (clang, clang++, linker, rustflags as link-args)
+SHARED_FLAGS_ARR=(
+    # Tells clang to compile for aarch64 linux
+    -target
+    aarch64-linux-gnu
+
+    # Jetson Nano CPU
+    -mcpu=cortex-a57
+
+    # Tells clang to link with lld, which also supports -target aarch64-linux-gnu
+    -fuse-ld=lld
+
+    # Sysroot containing aarch64-linux-gnu libraries to link against
+    --sysroot=$JETSON_SYSROOT
+
+    # CUDA libraries are here in sysroot from jetson
+    -L$JETSON_SYSROOT/usr/local/cuda-10.2/targets/aarch64-linux/lib/
+
+    # Custom build of OpenCV is installed here on jetson sysroot
+    -L$JETSON_SYSROOT/opt/opencv-4.6.0/lib/
+)
+
+# Only to clang to compile C code
+CFLAGS_ARR=(
+    ${SHARED_FLAGS_ARR[@]}
+)
+
+# Only to clant++ to compile C++ code
+CXXFLAGS_ARR=(
+    ${SHARED_FLAGS_ARR[@]}
+)
+
+# To linker
+LDFLAGS_ARR=(
+    ${SHARED_FLAGS_ARR[@]}
+)
+################################################################################
 
 
 ################################################################################
 # Environment setup
 ################################################################################
-# Make sure toolchain binaries are found in path!
-export PATH=$JETSON_TOOLCHAIN/bin:$PATH
-
-# Make sure any C/C++ code built by crates uses right compilers
-export CC=${JETSON_TOOLCHAIN_PREFIX}gcc
-export CXX=${JETSON_TOOLCHAIN_PREFIX}g++
+# Make sure any C/C++ code built by crates uses right compilers / flags
+export CC=clang
+export CXX=clang++
+export CFLAGS="$(IFS=" "; echo "${CFLAGS_ARR[*]}")"
+export CXXFLAGS="$(IFS=" "; echo "${CXXFLAGS_ARR[*]}")"
+export LDFLAGS="$(IFS=" "; echo "${LDFLAGS_ARR[*]}")"
+# Note: For C/C++ built by CMake, may need CMAKE_TOOLCHAIN_FILE variable
+# and a custom toolchain file. Maybe not. It may respect these vars too.
 ################################################################################
-
-
-
-################################################################################
-# Rust flags
-################################################################################
-# Search paths for libraries (IN SYSROOT!!!)
-lib_search_paths=(
-    # Standard search paths
-    $JETSON_SYSROOT/usr/lib/aarch64-linux-gnu/
-    $JETSON_SYSROOT/lib/aarch64-linux-gnu/
-    $JETSON_SYSROOT/usr/local/lib/aarch64-linux-gnu/
-
-    # CUDA libraries are here
-    $JETSON_SYSROOT/usr/local/cuda-10.2/targets/aarch64-linux/lib/
-
-    # Custom build of OpenCV is installed here
-    $JETSON_SYSROOT/opt/opencv-4.6.0/lib/
-)
-rustflags=""
-for path in "${lib_search_paths[@]}"; do
-    rustflags="$rustflags -C link-args=-Wl,-rpath-link,$path"
-done
-rustflags="${rustflags:1}"
-
-# Add Sysroot flag AFTER rpath-link flags
-rustflags="$rustflags -C link-args=-Wl,--sysroot=$JETSON_SYSROOT"
-################################################################################
-
 
 
 ################################################################################
 # Cargo flags / tools setup for target
 ################################################################################
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=${JETSON_TOOLCHAIN_PREFIX}gcc
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_AR=${JETSON_TOOLCHAIN_PREFIX}ar
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS="$rustflags"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=clang
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_AR=llvm-ar
+CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS=""
+for arg in "${SHARED_FLAGS_ARR[@]}"; do
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS="$CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS -C link-args=$arg"
+done
+CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS="${CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS:1}"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS
 ################################################################################
 
 
@@ -76,7 +93,6 @@ export OPENCV_LINK_PATHS="$JETSON_SYSROOT/opt/opencv-4.6.0/lib/"
 export OPENCV_INCLUDE_PATHS="$JETSON_SYSROOT/opt/opencv-4.6.0/include/opencv4"
 export OPENCV_DISABLE_PROBES="pkg_config,cmake,vcpkg_cmake,vcpkg"
 ################################################################################
-
 
 
 ################################################################################
